99dcf6940c53833e4aed45bc5cd9a24966ae9e30
machlord <mach.x@hotmail.com>
2021-07-19 18:45:56 -0300

    Implementando Repositorio central

--- api/Data/IRepository.cs
+++ api/Data/IRepository.cs
@@ -1,7 +1,12 @@
+using System.Threading.Tasks;
+
 namespace api.Data
 {
     public interface IRepository
     {
-        
+        void Add<T>(T Entity) where T : class;
+        void Update<T>(T Entity) where T : class;
+        void Delete<T>(T Entity) where T : class;
+        Task<bool> SaveChangesAsync();
     }
 }
--- api/Data/Repository.cs
+++ api/Data/Repository.cs
@@ -1,7 +1,32 @@
+using System.Threading.Tasks;
+
 namespace api.Data
 {
     public class Repository : IRepository
     {
+        private readonly DataContext _context;
+        public Repository(DataContext context)
+        {
+            _context = context;
+        }
+        public void Add<T>(T Entity) where T : class
+        {
+            _context.Add(Entity);
+        }
 
+        public void Delete<T>(T Entity) where T : class
+        {
+            _context.Remove(Entity);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return (await _context.SaveChangesAsync()) > 0;
+        }
+
+        public void Update<T>(T Entity) where T : class
+        {
+            _context.Update(Entity);
+        }
     }
 }
