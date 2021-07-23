44d5699b7764fd09a986522042674c95158c70f9
machlord <mach.x@hotmail.com>
2021-07-22 23:14:15 -0300

    Adicionando ultimos testes

--- test/CaixaController.test.cs
+++ test/CaixaController.test.cs
@@ -1,30 +1,53 @@
+using System;
+using System.Threading.Tasks;
+using api.Controllers;
+using api.Data;
+using api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
 using NUnit.Framework;
 
 namespace test
 {
-    public class CaixaControllerTest
+    public class CaixaControllerTest : IDisposable
     {
+        private DataContext _context;
         [SetUp]
         public void Setup()
         {
+            var options = new DbContextOptionsBuilder<DataContext>()
+                    .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                    .Options;
+            _context = new DataContext(options);
+            var caixas = new[]{
+                new Caixa {id = 1},
+                new Caixa {id = 2},
+                new Caixa {id = 3},
+                new Caixa {id = 4}
+            };
+            _context.Caixas.AddRange(caixas);
+            _context.SaveChanges();
         }
 
         [Test]
-        public void get_pega_todos_os_caixas()
+        public async Task get_pega_todos_os_caixas()
         {
-            Assert.Pass();
+            //Arrange
+            var CaixaController = new CaixaController(_context);
+
+            //Act
+            var actionResult = await CaixaController.Get();
+            var okResult = actionResult.Value;
+
+            //Assert
+            Assert.AreEqual(4, okResult.Count);
         }
 
-        [Test]
-        public void desativar_retorna_caixa_desativo()
+        public void Dispose()
         {
-            Assert.Pass();
-        }
-
-        [Test]
-        public void ativar_retorna_caixa_ativo()
-        {
-            Assert.Pass();
+            _context.Database.EnsureDeleted();
+            _context.Dispose();
         }
     }
 }
